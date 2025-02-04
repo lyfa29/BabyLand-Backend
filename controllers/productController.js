85daee53b1babcee0e7620a61dd30b7d60a6ea35
Sneaky29 <mac@MACs-MacBook-Pro.local>
2025-02-04 03:17:48 -0800

    ratings

--- controllers/productController.js
+++ controllers/productController.js
@@ -25,4 +25,38 @@ const getProductById = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllProducts, getProductById };
+//========================
+const rateProduct = async (req, res, next) => {
+  const { id } = req.params;
+
+  const { rating } = req.body;
+
+  if (!rating || rating < 1 || rating > 5) {
+    return res.status(400).json({ message: "Rating must be between 1 and 5" });
+  }
+
+  try {
+    const product = await product.findById(id);
+
+    if (!product) {
+      return res.staus(404).json({ message: "Product not Found" });
+    }
+
+    product.ratings.count += 1;
+
+    product.ratings.sum += rating;
+
+    product.ratings.average = product.ratings.sum / product.ratings.count;
+
+    await product.save();
+
+    res.status(200).json({
+      message: "Rating submitted Successfully",
+      updatedRatings: product.ratings,
+    });
+  } catch (error) {
+    next(err);
+  }
+};
+
+module.exports = { getAllProducts, getProductById, rateProduct };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getAllProducts,
   getProductById,
+  rateProduct,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
+router.post("/:id/rate", rateProduct);
 
 module.exports = router;
